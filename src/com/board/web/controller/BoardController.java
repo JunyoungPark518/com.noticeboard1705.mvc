920f3c98198c7327a56e52e9a909ddff558e23b3
pakjkwan <pakjkwan@gmail.com>
2017-05-25 14:53:48 +0900

    오후 2:53 2017-05-25

--- src/com/board/web/controller/BoardController.java
+++ src/com/board/web/controller/BoardController.java
@@ -21,8 +21,6 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import com.board.web.domain.ArticleBean;
 import com.board.web.service.BoardService;
 import com.board.web.serviceImpl.BoardServiceImpl;
-
-
 /**
 UPLOAD_DIRECTORY: name of the directory on the server where upload file will be stored. 
 					The directory is chosen to be relative to the web application’s directory.
@@ -40,24 +38,19 @@ MAX_REQUEST_SIZE: specifies the maximum size of a HTTP request which contains th
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String UPLOAD_DIRECTORY = "upload";
-	private static final String VIEW_DIRECTORY   = "/WEB-INF/views/"; 
+	private static final String VIEW_DIRECTORY   = "/WEB-INF/views/";
+	private static final String SAVE_PATH   = "C:\\Users\\hb2000\\JavaProjects\\eclipse4class14\\workspace\\noticeboard1705mvc\\WebContent\\resources\\upload\\";
+	private static final String SAVE_DIRECTORY   = "upload"; 
 	private static final int MAX_MEMORY_SIZE     = 1024 * 1024 * 3;  // 3MB
 	private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
 	private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
-	
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		String path = request.getServletPath(), directory = path.substring(0, path.indexOf(".")),
 				action = request.getParameter("action"), foo = request.getParameter("pageNumber"),
 				pageName = request.getParameter("pageName"), title = "", content = "";
-		if (action == null) {
-			action = "list";
-		}
-		if (foo == null) {
-			foo = "0";
-		}
-		System.out.println("ACTION :"+action);
+		action=(action == null) ? "list" : action;
+		foo=(foo==null)?"0":foo;	
 		String view = VIEW_DIRECTORY + directory + "/" + action + ".jsp";
 		BoardService service = BoardServiceImpl.getInstance();
 		ArticleBean bean = new ArticleBean();
@@ -73,8 +66,9 @@ public class BoardController extends HttpServlet {
 		HashMap<String, Object> param = new HashMap<>();
 		switch (action) {
 		case "move":
-			view = VIEW_DIRECTORY + directory + "/" + pageName + ".jsp";
-			request.getRequestDispatcher(view).forward(request, response);
+			request
+			.getRequestDispatcher(VIEW_DIRECTORY + directory + "/" + pageName + ".jsp")
+			.forward(request, response);
 			break;
 		case "list":
 			List<ArticleBean> list = service.findArticles(param);
@@ -85,87 +79,47 @@ public class BoardController extends HttpServlet {
 			request.setAttribute("startPage", startPage);
 			request.setAttribute("endPage", endPage);
 			request.setAttribute("pageNumber", pageNumber);
-			request.getRequestDispatcher(view).forward(request, response);
+			request
+			.getRequestDispatcher(VIEW_DIRECTORY + directory + "/list.jsp")
+			.forward(request, response);
 			break;
 		case "write": 
-			System.out.println("write enter");
 			request
 			.getRequestDispatcher(VIEW_DIRECTORY + directory + "/upload.jsp")
 			.forward(request, response);
 			break;
 		case "upload":
-			System.out.println("upload enter");
-		
-			
-			Map<String, String> map = new HashMap<String, String>();
-			view = VIEW_DIRECTORY + directory + "/" + "detail2" + ".jsp";
-			System.out.println("WRITE ENTER ServletContext :"+getServletContext());
-			System.out.println("WRITE ENTER RealPath :"+getServletContext().getRealPath(""));
-			String realPath="C:\\Users\\hb2000\\JavaProjects\\eclipse4class14\\workspace\\noticeboard1705mvc\\WebContent\\resources\\upload\\";
-			// checks if the request actually contains upload file
-	        if (!ServletFileUpload.isMultipartContent(request)) {
+	        String savePath=SAVE_PATH;
+	        System.out.println("WRITE ENTER RealPath :"+savePath);
+			if (!ServletFileUpload.isMultipartContent(request)) {
 	        	System.out.println("ServletFileUpload is Null");
 	            return;
 	        }
-	        System.out.println("ServletFileUpload is Not Null");  
-	        // configures upload settings
 	        FileItemFactory factory = new DiskFileItemFactory();
-	        ServletContext context=this.getServletConfig().getServletContext();
-	        File repository=(File) context.getAttribute("javax.servlet.context.tempdir");
-	/*        factory.setRepository(repository);
-	        factory.setSizeThreshold(MAX_MEMORY_SIZE);*/
 	        ServletFileUpload upload = new ServletFileUpload(factory);
 	        upload.setFileSizeMax(MAX_FILE_SIZE);
 	        upload.setSizeMax(MAX_REQUEST_SIZE);
 	        List<FileItem> items=null;
-	        // constructs the directory path to store upload file
-	     //   String uploadPath = getServletContext().getRealPath("")+ File.separator + UPLOAD_DIRECTORY;
-	        // creates the directory if it does not exist
-	        System.out.println("uploadPath :"+realPath);
 	        try {
-	        	System.out.println("try after!!!!!!!!!");
-	    
+	        	File file ;
 	        	items = upload.parseRequest( request); 
-	            System.out.println("List<FileItem> items"+items);
 	            Iterator<FileItem> iter = items.iterator();
-	            System.out.println("Iterator<?> iter ???"+iter.hasNext());
-	            // iterates over form's fields
-	            System.out.println("items.iterator() : iterator");
 	            while (iter.hasNext()) {
-	            	System.out.println("########while inner iter.hasNext()");
 	                FileItem item = (FileItem) iter.next();
-	                // processes only fields that are not form fields
 	                if (!item.isFormField()) {
-	                	map.put(item.getFieldName(), item.getString());
-	                 //   String fileName = new File(item.getName()).getName();
-	                 //   String filePath = realPath + File.separator + fileName;
-	                 //   String filePath = realPath + fileName;
-	                 //   File storeFile = new File(filePath);
-	                     
-	                    // saves the file on disk
-	                  //  item.write(storeFile);
-	                } else if (item.getFieldName().startsWith("file_workflow")) {
-
-	                	map.put(item.getFieldName(), new String(item.get()));
-
-	    				// uploaded file
+	                        String fieldName = item.getFieldName();
+	                        String fileName = item.getName();
+	                        boolean isInMemory = item.isInMemory();
+	                        long sizeInBytes = item.getSize();
+	                        file = new File( savePath + fileName) ;
+	                        item.write( file ) ;
 	    			} else {
-
-	    				// encode the uploaded file to base64
-	    				//String currentAttachment = new String(Base64.encode(item.get()));
-
-	    				// put the encoded attachment into the map
-	    				map.put(item.getFieldName(), "test");
+	    				System.out.println("item.isFormField() error");
 	    			}
 	            }
-	          //  request.setAttribute("message", "Upload has been done successfully!");
 	        } catch (Exception ex) {
 	        	System.out.println("fail");
-	          //  request.setAttribute("message", "There was an error: " + ex.getMessage());
 	        }
-	     //   getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
-	        System.out.println("view before@@@@@@@@");
-
 	        request
 			.getRequestDispatcher(VIEW_DIRECTORY + directory + "/detail.jsp")
 			.forward(request, response);
@@ -185,11 +139,9 @@ public class BoardController extends HttpServlet {
 			break;
 
 		case "delete":
-			System.out.println("delete entered form controller");
 			String deleteObect = request.getParameter("deleteObject");
 			bean.setSeqNo(deleteObect);
 			bean = service.deleteArticle(bean);
-			System.out.println("삭제 완료");
 			request.setAttribute("title", bean.getTitle());
 			request.setAttribute("content", bean.getContent());
 			request.setAttribute("writer", bean.getWriter());
@@ -199,12 +151,10 @@ public class BoardController extends HttpServlet {
 			.forward(request, response);
 			break;
 		case "detail":
-			System.out.println("delete entered form controller");
 			request
 			.getRequestDispatcher(VIEW_DIRECTORY + directory + "/detail.jsp")
 			.forward(request, response);
 			break;	
-
 		}
 	}
 }
